2ab9fe3ee973a5aec60805beb02c3b416ee3cc2a
MarionbDev <marionbaston84@gmail.com>
2023-11-24 21:12:56 +0100

    update styles and responsive design

--- app/_components/MyServices.jsx
+++ app/_components/MyServices.jsx
@@ -23,44 +23,83 @@ export default function MyServices() {
   return (
     <div className="flex flex-col items-center justify-center ">
       <div className="w-3/4 ">
-        <h2 className=" text-xl font-bold  sm:text-3xl w-full text-start border-b-2 pb-2 border-purple ">
+        <h2 className=" text-xl font-bold  sm:text-3xl w-full text-start border-b-2 mb-10 pb-2 border-purple ">
           Mes services
         </h2>
-        <p className="pt-0 md:pt-4 text-sm sm:text-base  ">
-          Ce que je propose !
-        </p>
-        <div className="flex flex-col items-center mt-4 ">
-          <div className=" shadow-purple/40 shadow-md  w-64 px-6 py-4 gap-6 flex flex-col items-center justify-center">
-            <h2 className="  lg:text-lg text-center">Developpeuse FrontEnd</h2>
-            <Image src={logoCode} alt="logo-code" width={90} />
-            <HoverCard className="flex flex-col items-center justify-centers ">
-              <HoverCardTrigger asChild>
-                <Button variant="link" className=" shadow-lg gap-4 w-full ">
-                  <p>Voir plus</p>
-                  <Image src={arrow} alt="arrow" width={24} />
-                </Button>
-              </HoverCardTrigger>
-              <HoverCardContent className="w-96  translate-x-72 -translate-y-20 shadow-purple/40 shadow-md border-none bg-purple/50 backdrop-blur-sm drop-shadow-lg rounded-lg ">
-                <div className="flex justify-between space-x-4">
-                  <div className="space-y-1">
-                    <h4 className="lg:text-base  font-semibold">
-                      Developpeuse FrontEnd
-                    </h4>
-                    <ul className=" lg:text-sm mt-8">
-                      <li>Responsive Web Design</li>
-                      <li>Interfaces utilisateurs interactives</li>
-                      <li>Optimisation SEO</li>
-                      <li>
-                        Accessibilité et amélioration de l'expérience
-                        utilisateur (UX)
-                      </li>
-                    </ul>
+
+        <div className="flex flex-col items-center mt-2 ">
+          <div className="flex justify-around gap-10">
+            <div className=" shadow-purple/40 shadow-md  w-64 px-6 py-4 gap-6 flex flex-col items-center justify-center">
+              <h2 className="  lg:text-lg text-center">
+                DEVELOPPEMENT WEB FRONTEND
+              </h2>
+              <Image src={logoCode} alt="logo-code" width={90} />
+              <HoverCard className="flex flex-col items-center justify-centers ">
+                <HoverCardTrigger asChild>
+                  <Button variant="link" className=" shadow-lg gap-4 w-full ">
+                    <p>Voir plus</p>
+                    <Image src={arrow} alt="arrow" width={24} />
+                  </Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-[30rem] h-44 overflow-y-scroll translate-x-[21rem] -translate-y-24 shadow-purple/40 shadow-md border-none bg-purple/20 backdrop-blur-sm drop-shadow-lg rounded-lg ">
+                  <div className="flex justify-between space-x-4">
+                    <div className="space-y-1 ">
+                      <h4 className="lg:text-base  font-semibold">
+                        Developpement Web FrontEnd
+                      </h4>
+                      <p className="lg:text-sm mt-8">
+                        Je développe l'interface visible de votre site web à
+                        partir d'une maquette.
+                      </p>
+                      <ul className=" lg:text-sm mt-8 list-disc pl-4 gap-1 flex flex-col">
+                        <li>Interfaces utilisateurs interactives</li>
+                        <li>
+                          Accessibilité et amélioration de l'expérience
+                          utilisateur (UX)
+                        </li>
+                        <li>Optimisation SEO</li>
+                        <li>
+                          Site web adaptable à tous les écrans (mobiles,
+                          tablettes, ordinateurs)
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                </div>
-              </HoverCardContent>
-            </HoverCard>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+            <div className=" shadow-purple/40 shadow-md  w-64 px-6 py-4 gap-6 flex flex-col items-center justify-center">
+              <h2 className="  lg:text-lg text-center">
+                DEVELOPPEMENT WEB BACKEND
+              </h2>
+              <Image src={logoCode} alt="logo-code" width={90} />
+              <HoverCard className="flex flex-col items-center justify-centers ">
+                <HoverCardTrigger asChild>
+                  <Button variant="link" className=" shadow-lg gap-4 w-full ">
+                    <p>Voir plus</p>
+                    <Image src={arrow} alt="arrow" width={24} />
+                  </Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-[30rem] h-44 overflow-y-scroll translate-x-[21rem] -translate-y-24 shadow-purple/40 shadow-md border-none bg-purple/20 backdrop-blur-3xl drop-shadow-lg rounded-lg ">
+                  <div className="flex justify-between space-x-4">
+                    <div className="space-y-1">
+                      <h4 className="lg:text-base  font-semibold">
+                        Developpement Web BackEnd
+                      </h4>
+                      <div className=" lg:text-sm mt-8">
+                        <p>
+                          Je réponds au cahier des charges en développant toutes
+                          les fonctionnalités invisibles à l'utilisateur et
+                          nécessaire au fonctionnement de l'interface.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
           </div>
-          <div className="  mt-24 w-3/5  ">
+          <div className="  mt-20 xl:w-3/5  ">
             <Card className="shadow-purple/40 shadow-xl border-none bg-purple/90 rounded-xl  text-[#ffffff] ">
               <CardHeader>
                 <CardTitle>
@@ -74,12 +113,12 @@ export default function MyServices() {
               <CardContent>
                 <Button
                   type="button"
-                  className="bg-[#ffffff]  hover:bg-[#baa8bbc0] rounded-xl p-2 text-[#313030] hover:text-[#ffff]  "
+                  className="bg-[#ffffff]  hover:bg-[#baa8bbc0] rounded-xl p-2 text-[#313030] hover:text-[#ffff]   shadow-xl"
                 >
                   <Link
                     href={`mailto:${emailAddress}`}
                     aria-labelledby="contact-me"
-                    className="flex gap-1 md:gap-2 text-[10px] text-center sm:text-sm text-purple"
+                    className="flex gap-1 md:gap-2 text-[10px] text-center sm:text-sm text-purple "
                   >
                     <MessagesSquare size="16" id="contact-me" />
                     Contactez-moi
