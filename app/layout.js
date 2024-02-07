95119f3e9b56a320950d3611f81f2a16b4640227
MarionbDev <marionbaston84@gmail.com>
2024-02-07 20:04:13 +0100

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  robots: {
+    index: true,
+    follow: true,
+  },
   title: "Portfolio Marion Baston | Développeuse Web",
   description:
     "Je suis Marion, développeuse web et web mobile passionnée par les nouvelles technologies, j'aime expérimenter, découvrir et apprendre. Spécialisée dans les frameworks REACT.js et NEXT.js. Découvrez mes compétences et projets sur mon site web.",
