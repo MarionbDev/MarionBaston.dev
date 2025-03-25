3b0bd3378e40e49270d8cbd0448884dbea28d0ad
MarionbDev <marionbaston84@gmail.com>
2025-03-25 21:25:00 +0100

    add component projectStepCard

--- app/freelance/page.jsx
+++ app/freelance/page.jsx
@@ -1,7 +1,7 @@
 import ContactMe from "./_components/ContactMe";
 import Header from "./_components/Header";
+import ProjectStep from "./_components/project-step/ProjectStep";
 import ProjectsFree from "./_components/ProjectsFree";
-import ProjectStep from "./_components/ProjectStep";
 import Services from "./_components/Services";
 import Support from "./_components/SupportFree";
 import Testimonials from "./_components/Testimonials";
@@ -10,7 +10,7 @@ import styles from "./styles.module.css";
 export default function FreelancePage() {
   return (
     <div
-      className={`${styles.spaceG} px-6 pt-10 flex flex-col  p sm:px-24 lg:px-28 text-[#322e37]`}
+      className={`${styles.spaceG} px-6 pt-10 flex flex-col sm:px-24 xl:px-28 text-[#322e37]`}
     >
       <Header />
       <div className="flex flex-col gap-24">
